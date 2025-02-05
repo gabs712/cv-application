aa44d02f254e54952956e2ac4f755dad0bac4fed
gabs <gabs712.g@gmail.com>
2025-02-05 18:32:39 -0300

    Add initial cv info

---
+++ src/data/initialCvInfo.js
@@ -0,0 +1,54 @@
+export default {
+  name: 'Josh Torvalds',
+  email: 'josh.dev@gmail.com',
+  phone: '+16 9503 1239 9021',
+  education: [
+    {
+      id: 1,
+      school: 'Harvard University',
+      formation: 'Bachelors in Computer Science',
+      timePeriod: '02/2017 - 03/2022',
+    },
+    {
+      id: 2,
+      school: 'MIT',
+      formation: 'Masters in Software Engineering',
+      timePeriod: '09/2022 - 06/2024',
+    },
+    {
+      id: 3,
+      school: 'Stanford University',
+      formation: 'Advanced Web Development Certification',
+      timePeriod: '07/2024 - 12/2024',
+    },
+  ],
+  experience: [
+    {
+      id: 1,
+      company: 'Microsoft',
+      position: 'Front-end Developer',
+      timePeriod: '06/2023 - present',
+      address: 'Poland, Warszawa',
+      description:
+        'Developed and maintained responsive user interfaces, optimized performance, collaborated with designers and backend developers, implemented accessibility best practices, conducted code reviews, and ensured seamless user experiences using modern web technologies. ',
+    },
+    {
+      id: 2,
+      company: 'Google',
+      position: 'UI Engineer',
+      timePeriod: '04/2022 - 05/2023',
+      address: 'United Kingdom, London',
+      description:
+        'Designed and developed scalable UI components, improved application performance, worked closely with UX designers, and implemented front-end best practices to enhance user experience.',
+    },
+    {
+      id: 3,
+      company: 'Amazon',
+      position: 'Web Developer Intern',
+      timePeriod: '06/2021 - 09/2021',
+      address: 'United States, Seattle',
+      description:
+        'Assisted in developing web applications, wrote reusable components, performed debugging, and contributed to front-end optimizations for e-commerce platforms.',
+    },
+  ],
+}
