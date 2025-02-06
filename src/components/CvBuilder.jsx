a666fcf7e88d491d1beff3f95540f087dcc1c451
gabs <gabs712.g@gmail.com>
2025-02-06 15:22:41 -0300

    Add state for CvBuilder

--- src/components/CvBuilder.jsx
+++ src/components/CvBuilder.jsx
@@ -1,12 +1,19 @@
-import initialCvInfo from '../data/initialCvInfo'
+import { useImmer } from 'use-immer'
+import initialData from '../data/initialCvInfo'
 import Cv from './Cv'
 import Form from './Form'
 
 export default function CvBuilder() {
+  const [data, updateData] = useImmer(initialData)
+
+  const handleChange = (cb) => () => {
+    updateData(cb)
+  }
+
   return (
-    <div>
-      <Form />
-      <Cv />
-    </div>
+    <main className="grid">
+      <Form {...{ data, handleChange, initialData }} />
+      <Cv data={data} />
+    </main>
   )
 }
