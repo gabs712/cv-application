75731c327738be15452840f8f70341e49d61faa2
gabs <gabs712.g@gmail.com>
2025-02-06 14:10:15 -0300

    Add max width on container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ export default function Container({ children }) {
   return (
     <div className="relative min-h-svh bg-slate-100">
       <div className="absolute top-0 min-h-[68svh] w-full bg-sky-500 shadow-sm"></div>
-      <div className="relative mx-auto w-full">{children}</div>
+      <div className="relative mx-auto max-w-7xl w-full">{children}</div>
     </div>
   )
 }
