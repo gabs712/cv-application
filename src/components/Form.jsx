edfc9f270cdb8aaa668ba30ad41015d684cb544c
gabs <gabs712.g@gmail.com>
2025-02-08 17:02:19 -0300

    Decouple section from form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-function Input({ label, initial, handleChange }) {
+function renameLater({ label, initial, handleChange }) {
   return (
     <div>
       <label>
@@ -15,37 +15,47 @@ function Input({ label, initial, handleChange }) {
   )
 }
 
+function Field() {
+  //
+}
+
+function Section({ title, fields }) {
+  return (
+    <div>
+      <h2 className="font-bold" key={title}>
+        {title}
+      </h2>
+
+      {fields.map((section, sectionIndex) =>
+        Object.entries(section)
+          .filter(([label]) => label !== 'id')
+          .map(([label, input]) => {
+            function handleChange(newValue) {
+              updateData((draft) => {
+                draft[title][sectionIndex][label] = newValue
+              })
+            }
+
+            return (
+              <Field
+                key={label}
+                label={label}
+                handleChange={handleChange}
+                initial={initialData[title][sectionIndex][label]}
+                input={input}
+              />
+            )
+          }),
+      )}
+    </div>
+  )
+}
+
 export default function Form({ data, updateData, initialData }) {
   return (
     <form>
-      {Object.entries(data).map(([title, sections]) => (
-        <div key={title}>
-          <h2 className="font-bold" key={title}>
-            {title}
-          </h2>
-
-          {sections.map((section, sectionIndex) =>
-            Object.entries(section)
-              .filter(([label]) => label !== 'id')
-              .map(([label, input]) => {
-                function handleChange(newValue) {
-                  updateData((draft) => {
-                    draft[title][sectionIndex][label] = newValue
-                  })
-                }
-
-                return (
-                  <Input
-                    key={label}
-                    label={label}
-                    handleChange={handleChange}
-                    initial={initialData[title][sectionIndex][label]}
-                    input={input}
-                  />
-                )
-              }),
-          )}
-        </div>
+      {Object.entries(data).map(([title, fields]) => (
+        <Section key={title} title={title} fields={fields} />
       ))}
     </form>
   )
